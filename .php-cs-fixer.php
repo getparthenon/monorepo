374163335f4a4dc59ae234e07c478c51f2b1c716
Iain Cambridge <iain@humblyarrogant.io>
2022-10-01 16:25:45 +0200

    Add cs config file

---
+++ .php-cs-fixer.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Humbly Arrogant Ltd 2020-2022.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: 01-10-2025 ( 3 years after 2.0.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+return (new PhpCsFixer\Config())
+            ->setRiskyAllowed(true)
+            ->setRules([
+                '@PSR2' => true,
+                '@Symfony' => true,
+                'header_comment' => ['header' => 'Copyright Humbly Arrogant Ltd 2020-2022.
+                
+Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+
+Change Date: 01-10-2025 ( 3 years after 2.0.0 release )
+
+On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.'],
+                'list_syntax' => ['syntax' => 'short'],
+                'array_syntax' => ['syntax' => 'short'],
+                'declare_strict_types' => true,
+                'ordered_class_elements' => true,
+                'no_multiple_statements_per_line' => true,
+                'constant_case' => true,
+                'no_useless_nullsafe_operator' => true,
+            ])
+            ->setFinder(
+                PhpCsFixer\Finder::create()->in(__DIR__)
+            )
+;
