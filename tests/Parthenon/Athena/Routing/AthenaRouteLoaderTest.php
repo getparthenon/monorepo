d6fd738a301b99fcf4b8efd111d5b7a3512d0f7d
Iain Cambridge <iain@humblyarrogant.io>
2022-11-20 22:43:50 +0100

    athena revert routing test

--- tests/Parthenon/Athena/Routing/AthenaRouteLoaderTest.php
+++ tests/Parthenon/Athena/Routing/AthenaRouteLoaderTest.php
@@ -29,6 +29,6 @@ class AthenaRouteLoaderTest extends TestCase
 
         $athenaRouteLoader = new AthenaRouteLoader($sectionManager, null);
         $routes = $athenaRouteLoader->load('mixed');
-        $this->assertCount(9, $routes);
+        $this->assertCount(10, $routes);
     }
 }
