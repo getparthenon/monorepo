8780703076944fb64c149675051e30eb5dd1a6be
Iain Cambridge <iain@humblyarrogant.io>
2022-11-27 18:05:33 +0100

    Add uploadString

---
+++ src/Parthenon/Export/Messenger/BackgroundExportRequestHandler.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2020-2022.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.1.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace Parthenon\Export\Messenger;
+
+use Parthenon\Common\LoggerAwareTrait;
+use Parthenon\Common\Upload\UploaderInterface;
+use Parthenon\Export\DataProvider\DataProviderFetcher;
+use Parthenon\Export\Entity\BackgroundExportRequest;
+use Parthenon\Export\Exporter\ExporterManagerInterface;
+use Parthenon\Export\Normaliser\NormaliserManagerInterface;
+use Parthenon\Export\Repository\BackgroundExportRequestRepositoryInterface;
+use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+
+class BackgroundExportRequestHandler implements MessageHandlerInterface
+{
+    use LoggerAwareTrait;
+
+    public function __construct(
+        private BackgroundExportRequestRepositoryInterface $backgroundExportRequestRepository,
+        private DataProviderFetcher $dataProviderFetcher,
+        private ExporterManagerInterface $exporterManager,
+        private NormaliserManagerInterface $normaliserManager,
+        private UploaderInterface $uploader,
+    ) {
+    }
+
+    public function __invoke(BackgroundExportRequest $message)
+    {
+        $this->getLogger()->info('Processing background export request');
+
+        $backgroundExportRequest = $this->backgroundExportRequestRepository->findById($message->getId());
+
+        $dataProvider = $this->dataProviderFetcher->getDataProvider($backgroundExportRequest);
+
+        $exporter = $this->exporterManager->getExporter($backgroundExportRequest);
+        $data = $dataProvider->getData($backgroundExportRequest);
+        $normaliser = null;
+        $normalisedData = [];
+
+        foreach ($data as $item) {
+            // Done this way incase it's a generator.
+            if (!isset($normaliser)) {
+                $normaliser = $this->normaliserManager->getNormaliser($item);
+            }
+
+            $normalisedData[] = $normaliser->normalise($item);
+        }
+
+        $exportedContent = $exporter->getOutput($normalisedData);
+        $filename = $exporter->getFilename($backgroundExportRequest->getFilename());
+    }
+}
