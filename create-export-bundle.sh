2a20ec118777a31ac846f91d4d56b75cd346ba35
Iain Cambridge <iain@humblyarrogant.io>
2022-12-09 14:09:33 +0100

    Update script

--- create-export-bundle.sh
+++ create-export-bundle.sh
@@ -16,7 +16,6 @@ mkdir -p ../export-bundle/tests/Parthenon
 
 echo "[x] Update Common"
 cp -R src/Parthenon/Common ../export-bundle/src/
-cp src/Parthenon/DependencyInjection/Modules/Common.php ../export-bundle/src/DependencyInjection/Modules/Common.php
 cp -R src/Parthenon/Resources/config/doctrine-mapping/Common ../export-bundle/src/Resources/config/doctrine-mapping/
 cp -R src/Parthenon/Resources/config/services/common/pdf ../export-bundle/src/Resources/config/services/common/pdf
 cp -R src/Parthenon/Resources/config/services/common.xml ../export-bundle/src/Resources/config/services/common.xml
@@ -25,7 +24,6 @@ cp -R tests/Parthenon/Common ../export-bundle/tests/Parthenon/
 
 echo "[x] Update Export"
 cp -R src/Parthenon/Export ../export-bundle/src/
-cp src/Parthenon/DependencyInjection/Modules/Export.php ../export-bundle/src/DependencyInjection/Modules/Export.php
 cp -R src/Parthenon/Resources/config/doctrine-mapping/Export ../export-bundle/src/Resources/config/doctrine-mapping/Export
 cp -R src/Parthenon/Resources/config/services/orm/export.xml ../export-bundle/src/Resources/config/services/orm/export.xml
 cp -R src/Parthenon/Resources/config/services/odm/export.xml ../export-bundle/src/Resources/config/services/odm/export.xml
@@ -35,7 +33,7 @@ cp -R tests/Parthenon/Export ../export-bundle/tests/Parthenon/
 
 echo "[x] Update Notification"
 cp -R src/Parthenon/Notification ../export-bundle/src/
-cp src/Parthenon/DependencyInjection/Modules/Notification.php ../export-bundle/src/DependencyInjection/Modules/Notification.php
+cp -R src/Parthenon/DependencyInjection/Modules/Notification.php ../export-bundle/src/DependencyInjection/Modules/Notification.php
 cp -R src/Parthenon/Resources/config/services/notification.xml ../export-bundle/src/Resources/config/services/notification.xml
 cp -R tests/Parthenon/Notification ../export-bundle/tests/Parthenon/
 
