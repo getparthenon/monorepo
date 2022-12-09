962f131915be2707babdf531defc7d0fe1267b5c
Iain Cambridge <iain@humblyarrogant.io>
2022-12-09 10:57:44 +0100

    Add export bundle update

---
+++ create-export-bundle.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+echo "Starting creation of export bundle"
+
+echo "[x] Removing old files"
+rm -Rf ../export-bundle/src/Common/*
+rm -Rf ../export-bundle/src/Export/*
+rm -Rf ../export-bundle/src/DependencyInjection/*
+rm -Rf ../export-bundle/src/Resources/config/services/*
+mkdir ../export-bundle/src/Resources/config/services/orm/
+mkdir ../export-bundle/src/Resources/config/services/odm/
+mkdir ../export-bundle/src/Resources/config/services/common/
+mkdir -p ../export-bundle/tests/Parthenon
+
+cp -R src/Parthenon/DependencyInjection ../export-bundle/src
+cp src/Parthenon/ParthenonBundle.php ../export-bundle/src/ParthenonBundle.php
+
+
+echo "[x] Update Common"
+cp -R src/Parthenon/Common ../export-bundle/src/
+cp src/Parthenon/DependencyInjection/Modules/Common.php ../export-bundle/src/DependencyInjection/Modules/Common.php
+cp -R src/Parthenon/Resources/config/doctrine-mapping/Common ../export-bundle/src/Resources/config/doctrine-mapping/
+cp -R src/Parthenon/Resources/config/services/common/pdf ../export-bundle/src/Resources/config/services/common/pdf
+cp -R src/Parthenon/Resources/config/services/common.xml ../export-bundle/src/Resources/config/services/common.xml
+cp -R tests/Parthenon/Common ../export-bundle/tests/Parthenon/
+
+echo "[x] Update Export"
+cp -R src/Parthenon/Export ../export-bundle/src/
+cp src/Parthenon/DependencyInjection/Modules/Export.php ../export-bundle/src/DependencyInjection/Modules/Export.php
+cp -R src/Parthenon/Resources/config/doctrine-mapping/Export ../export-bundle/src/Resources/config/doctrine-mapping/Export
+cp -R src/Parthenon/Resources/config/services/orm/export.xml ../export-bundle/src/Resources/config/services/orm/export.xml
+cp -R src/Parthenon/Resources/config/services/odm/export.xml ../export-bundle/src/Resources/config/services/odm/export.xml
+cp -R src/Parthenon/Resources/config/services/export.xml ../export-bundle/src/Resources/config/services/export.xml
+cp -R tests/Parthenon/Export ../export-bundle/tests/Parthenon/
+
+cd ../export-bundle
+git add -A
+if [ -z "$(git status --porcelain)" ]; then
+  echo "No changes found"
+else
+  echo "Pushing update to repository"
+  git commit -am "$COMMIT_MESSAGE
+
+
+Automatic commit for https://github.com/getparthenon/monorepo/commit/$GITHUB_SHA"
+  git push origin $BRANCH
+fi
