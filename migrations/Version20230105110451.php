11f07b3fac85617d1d986cda54bcd95fa1db2ae5
Iain Cambridge <iain@humblyarrogant.io>
2023-01-05 12:05:21 +0100

    [Billing] Add created at and deleted to payment details

---
+++ migrations/Version20230105110451.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2020-2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.2.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230105110451 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE payment_details ADD is_deleted BOOLEAN NOT NULL');
+        $this->addSql('ALTER TABLE payment_details ADD created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE payment_details DROP is_deleted');
+        $this->addSql('ALTER TABLE payment_details DROP created_at');
+    }
+}
