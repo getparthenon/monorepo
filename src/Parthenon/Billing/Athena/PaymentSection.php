ae223c70ece5a6289289d614e368a7fa77a90dc8
Iain <iain@humblyarrogant.io>
2023-03-09 20:01:31 +0100

    [Billing] Add payments athena section

--- src/Parthenon/Billing/Athena/PaymentSection.php
+++ src/Parthenon/Billing/Athena/PaymentSection.php
@@ -1,5 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * Copyright Iain Cambridge 2020-2023.
+ *
+ * Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
+ *
+ * Change Date: TBD ( 3 years after 2.2.0 release )
+ *
+ * On the date above, in accordance with the Business Source License, use of this software will be governed by the open source license specified in the LICENSE file.
+ */
+
 namespace Parthenon\Billing\Athena;
 
 use Parthenon\Athena\AbstractSection;
@@ -10,7 +22,9 @@ use Parthenon\Billing\Repository\PaymentRepositoryInterface;
 
 class PaymentSection extends AbstractSection
 {
-    public function __construct(private PaymentRepositoryInterface $paymentRepository) {}
+    public function __construct(private PaymentRepositoryInterface $paymentRepository)
+    {
+    }
 
     public function getUrlTag(): string
     {
