17abe5830c304150ca04bd4a51146bb2e5deb89c
Iain <iain@humblyarrogant.io>
2024-01-03 22:30:10 +0100

    Update copyright header

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -17,7 +17,7 @@ return (new PhpCsFixer\Config())
             ->setRules([
                 '@PSR2' => true,
                 '@Symfony' => true,
-                'header_comment' => ['header' => 'Copyright Humbly Arrogant Software Limited 2020-2023.
+                'header_comment' => ['header' => 'Copyright Humbly Arrogant Software Limited 2020-2024
                 
 Use of this software is governed by the Business Source License included in the LICENSE file and at https://getparthenon.com/docs/next/license.
 
